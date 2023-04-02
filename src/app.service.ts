1a4ae91bd4307f822c73b2904778e4187707b1f1
Grigoriev Aleksander <akr4log@gmail.com>
2023-04-02 17:04:20 +0300

    UPD

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello САНЯ!';
+    return 'Это работает при изменении кода!!!';
   }
 }
